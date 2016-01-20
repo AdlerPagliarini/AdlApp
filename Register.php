965bd6b37ffff8656c1e283b76c0392ccc160f1e
AdlerPagliarini <adlerpagliarini@gmail.com>
2016-01-20 07:23:11 -0200

    phpFiles
    Conexão com o banco de dados para login... Register.php registra um novo
    usuário, FetchUserData.php retorna um usuário.

---
+++ Register.php
@@ -0,0 +1,16 @@
+<?php
+    $con=mysqli_connect("localhost:3306","tagcode","android@mySQL","androidmySQL");
+    
+    $name = $_POST["name"];
+    $age = $_POST["age"];
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    
+    $statement = mysqli_prepare($con, "INSERT INTO tUser (name, age, username, password) VALUES (?, ?, ?, ?)");
+    mysqli_stmt_bind_param($statement, "siss", $name, $age, $username, $password);
+    mysqli_stmt_execute($statement);
+    
+    mysqli_stmt_close($statement);
+    
+    mysqli_close($con);
+?>
